1805959a4bf7e737c1b76bff9c37b42a6b0a1a42
federica <federicazanca8@gmail.com>
2024-03-05 14:15:16 +0000

    refine submission script

--- aiida_mlip/calculations/singlepoint.py
+++ aiida_mlip/calculations/singlepoint.py
@@ -2,7 +2,7 @@
 
 from aiida.common import datastructures
 from aiida.engine import CalcJob, CalcJobProcessSpec
-from aiida.orm import Dict, Str, StructureData
+from aiida.orm import Dict, SinglefileData, Str, StructureData
 
 from aiida_mlip.data.model import ModelData
 
@@ -12,7 +12,6 @@ class Singlepoint(CalcJob):
 
     _DEFAULT_INPUT_FILE = "aiida.cif"
     _DEFAULT_OUTPUT_FILE = "aiida.xyz"
-    _DEFAULT_LOG_FILE = "aiida.log"
 
     @classmethod
     def define(cls, spec: CalcJobProcessSpec):
@@ -23,10 +22,22 @@ class Singlepoint(CalcJob):
         super().define(spec)
 
         # Define inputs
-        spec.input("model", valid_type=ModelData)
-        spec.input("structure", valid_type=StructureData, help="The inputs structure.")
-        spec.input("precision", valid_type=Str)
-        spec.input("device", valid_type=Str)
+        spec.input(
+            "calctype",
+            valid_type=Str,
+            help="calculation type (single point or geom opt)",
+        )
+        spec.input(
+            "model", valid_type=ModelData, help="mlip model used for calculation"
+        )
+        spec.input("structure", valid_type=StructureData, help="The input structure.")
+        spec.input("precision", valid_type=Str, help="Precision level for calculation")
+        spec.input(
+            "device",
+            valid_type=Str,
+            help="Device where to run calculation(cpu, gpu...)",
+        )
+        # additional arguments?
         spec.input(
             "metadata.options.input_filename",
             valid_type=str,
@@ -37,6 +48,14 @@ class Singlepoint(CalcJob):
             valid_type=str,
             default=cls._DEFAULT_OUTPUT_FILE,
         )
+
+        spec.input(
+            "metadata.options.scheduler_stdout",
+            valid_type=str,
+            default="_scheduler-stdout.txt",
+            help="Filename to which the content of stdout of the scheduler is written.",
+        )
+
         spec.inputs.validator = cls.validate_inputs
 
         # Outputs, in this case it would just be a dictionary with energy etc
@@ -45,6 +64,7 @@ class Singlepoint(CalcJob):
             valid_type=Dict,
             help="The `output_parameters` output node of the successful calculation.",
         )
+        spec.output(cls._DEFAULT_OUTPUT_FILE, valid_type=SinglefileData)
         # Input errors
         spec.exit_code(300, "INPUT_ERROR", message="Some problems reading the input")
         # Warnings
@@ -69,6 +89,7 @@ class Singlepoint(CalcJob):
         for key in ("model", "structure"):
             if key not in value:
                 return f"required value was not provided for the `{key}` namespace."
+
         return None
 
     def prepare_for_submission(self, folder):
@@ -85,31 +106,35 @@ class Singlepoint(CalcJob):
         aiida.common.datastructures.CalcInfo
             An instance of `aiida.common.datastructures.CalcInfo`.
         """
-        # Input parameters that we want to store in the config file
+        # Input parameters
+        calctype = self.inputs.calctype
         architecture = self.inputs.model.architecture
         model_path = self.inputs.model.file
         structure = self.inputs.structure
+        precision = self.inputs.precision
         cif_structure = structure.get_cif()
         device = self.inputs.device
 
         with folder.open(
-            "cls._DEFAULT_INPUT_FILE", "w"
+            self._DEFAULT_INPUT_FILE, "w"
         ) as inputfile:  # check better how the folder thing works
             inputfile.write(cif_structure.get_content())
 
         cif_file_path = folder / "aiida.cif"
 
+        # In future sp and opt, and kwargs?
         cmd_line = {
             "architecture": architecture,
             "model": model_path,
             "structure": cif_file_path,
             "device": device,
+            "precision": precision,
         }
 
         codeinfo = datastructures.CodeInfo()
 
         # adding command line params for when we run janus
-        codeinfo.cmdline_params = []
+        codeinfo.cmdline_params = [f" {calctype} "]
         for flag in cmd_line.items():
             codeinfo.cmdline_params += [f"--{flag[0]}", flag[1]]
 
--- examples/calculations/submit_singlepoint.py
+++ examples/calculations/submit_singlepoint.py
@@ -1,13 +1,16 @@
 """Example code for submitting single point calculation"""
 
+from pathlib import Path
 import sys
+from typing import Union
 
 from ase.build import bulk
+from ase.io import read
 import click
 
 from aiida.common import NotExistent
 from aiida.engine import submit
-from aiida.orm import Code, Str, StructureData
+from aiida.orm import Code, Str, StructureData, load_node
 from aiida.plugins import CalculationFactory, DataFactory
 
 from aiida_mlip.data.model import ModelData
@@ -15,34 +18,78 @@ from aiida_mlip.data.model import ModelData
 StructureData = DataFactory("structure")
 
 
-def singlepoint(code):
+def load_model(string, architecture):
+    """Given a string for the model, if the string is a path use that model otherwise download"""
+    file_path = Path(string)
+    if file_path.is_file():
+        model = ModelData.local_file(file_path, architecture=architecture)
+        return model
+    model = ModelData.download(string, architecture=architecture)
+    return model
+
+
+def load_structure(value):
+    """Load StructureData type when given a path, a node or None"""
+    if value is None:
+        structure = StructureData(ase=bulk("Si", "fcc", 5.43))
+        return structure
+    try:
+        # We try to convert the given value to an int, which should be the number of a node
+        structure_pk = int(value)
+        structure = load_node(structure_pk)
+        return structure
+    except ValueError as exc:
+        # If conversion to int fails, it should be a string
+        if Path.exists(value):
+            structure = StructureData(ase=read(value))
+            return structure
+        raise click.BadParameter(
+            f"Invalid input: {value}. Must be either node PK (int) or a valid path to a structure file."
+        ) from exc
+
+
+def singlepoint(params):
     """Prepare inputs and run singlepoint calculation"""
-    # Example Si structure
-    structure = StructureData(ase=bulk("Si", "fcc", 5.43))
-    # Structure can be any path to a structure
-    # structure = StructureData(ase=ase.io.read(Path("path/to/ciffile.cif")))
+    structure = load_structure(params["file"])
 
     # Select model to use
-    model = ModelData.local_file("path/to/model", architecture="mace_mp")
+    model = load_model(params["model"], params["architecture"])
 
     # Select calculation to use
     Singlepointcalc = CalculationFactory("aiida_mlip.calculations.singlepoint")
 
     inputs = {
         "structure": structure,
+        "calctype": Str(params["calctype"]),
         "model": model,
-        "precision": Str("float64"),
-        "device": Str("cpu"),
-        "code": code,
+        "precision": Str(params["precision"]),
+        "device": Str(params["device"]),
+        "code": params["code"],
     }
 
+    # Submit calculation
     submit(Singlepointcalc, **inputs)
 
 
 # Arguments and options to give to the cli when running the script
 @click.command("cli")
 @click.argument("codelabel", type=str)
-def cli(codelabel):
+@click.option("--calctype", default="singlepoint", type=str)
+@click.option(
+    "--file",
+    default=None,
+    type=Union[str, int],
+    help="Specify the structure (aiida node or path to a structure file)",
+)
+@click.option(
+    "--model", default=None, type=str, help="Specify path or url of the model to use"
+)
+@click.option("--architecture", default="mace_mp", type=str)
+@click.option("--device", default="cpu", type=str)
+@click.option("--precision", default="float64", type=str)
+def cli(
+    codelabel, calctype, file, model, architecture, device, precision
+):  # pylint: disable=too-many-arguments
     """Click interface."""
     try:
         code = Code.get_from_string(codelabel)
@@ -50,7 +97,18 @@ def cli(codelabel):
         print(f"The code '{codelabel}' does not exist.")
         sys.exit(1)
 
-    singlepoint(code)
+    params = {
+        "code": code,
+        "calctype": calctype,
+        "file": file,
+        "model": model,
+        "architecture": architecture,
+        "device": device,
+        "precision": precision,
+    }
+
+    # Submit single point to aiida
+    singlepoint(params)
 
 
 if __name__ == "__main__":
