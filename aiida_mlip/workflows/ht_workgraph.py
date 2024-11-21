ba739642f93957bf1a2ed0c1b3afb77a55347b66
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2024-11-21 15:29:16 +0100

    Generalise ht workgraph

--- aiida_mlip/workflows/ht_workgraph.py
+++ aiida_mlip/workflows/ht_workgraph.py
@@ -1,78 +1,112 @@
-"""Workgraph to run high-throughput geometry optimisations."""
+"""Workgraph to run high-throughput calculations."""
 
 from pathlib import Path
+from typing import Callable, Union
 
-from aiida.plugins import CalculationFactory
+from aiida.engine import CalcJob, WorkChain
 from aiida_workgraph import WorkGraph, task
 from ase.io import read
 
 from aiida_mlip.helpers.help_load import load_structure
 
-GeomoptCalc = CalculationFactory("mlip.opt")
 
-
-@task.graph_builder(outputs=[{"name": "final_structures", "from": "context.relax"}])
-def run_opt_calc(folder: Path, janus_opt_inputs: dict) -> WorkGraph:
+@task.graph_builder(outputs=[{"name": "final_structures", "from": "context.structs"}])
+def ht_calc_builder(
+    calc: Union[CalcJob, Callable, WorkChain, WorkGraph],
+    folder: Path,
+    calc_inputs: dict,
+    input_struct_key: str = "struct",
+    final_struct_key: str = "final_structure",
+) -> WorkGraph:
     """
-    Run a geometry optimisation using Geomopt.
+    Build high throughput calculation WorkGraph.
+
+    The `calc` must take a structure, by default `struct`, as one of its inputs.
+    Tasks will then be created for each structure file in `folder`.
 
     Parameters
     ----------
+    calc : Union[CalcJob, Callable, WorkChain, WorkGraph]
+        Calculation to be performed on all structures.
     folder : Path
         Path to the folder containing input structure files.
-    janus_opt_inputs : dict
-        Dictionary of inputs for the calculations.
+    calc_inputs : dict
+        Dictionary of inputs, shared by all the calculations. Must not contain
+        `struct_key`.
+    input_struct_key : str
+        Keyword for input structure for `calc`. Default is "struct".
+    final_struct_key : str
+        Key for final structure output from `calc`. Default is "final_structure".
 
     Returns
     -------
     WorkGraph
-        The workgraph containing the optimisation tasks.
+        The workgraph with calculation tasks for each structure.
     """
     wg = WorkGraph()
     for child in folder.glob("**/*"):
         try:
             read(child.as_posix())
-        except Exception:  # pylint: disable=broad-except
+        except Exception:
             continue
         structure = load_structure(child)
-        janus_opt_inputs["struct"] = structure
-        relax = wg.add_task(
-            GeomoptCalc,
-            name=f"relax_{child.stem}",
-            **janus_opt_inputs,
+        calc_inputs[f"{input_struct_key}"] = structure
+        calc_task = wg.add_task(
+            calc,
+            name=f"calc_{child.stem}",
+            **calc_inputs,
         )
-        relax.set_context({"final_structure": f"relax.{child.stem}"})
+        calc_task.set_context({f"{final_struct_key}": f"structs.{child.stem}"})
     return wg
 
 
-def ht_workgraph(folder_path: Path, inputs: dict) -> WorkGraph:
+def get_ht_workgraph(
+    calc: Union[CalcJob, Callable, WorkChain, WorkGraph],
+    folder: Path,
+    calc_inputs: dict,
+    input_struct_key: str = "struct",
+    final_struct_key: str = "final_structure",
+    max_number_jobs: int = 10,
+) -> WorkGraph:
     """
-    Create and execute a high-throughput workflow for geometry optimisation using MLIPs.
+    Get high throughput workflow for calculation.
 
     Parameters
     ----------
-    folder_path : Path
+    calc : Union[CalcJob, Callable, WorkChain, WorkGraph]
+        Calculation to be performed on all structures.
+    folder : Path
         Path to the folder containing input structure files.
-    inputs : dict
-        Dictionary of inputs for the calculations.
+    calc_inputs : dict
+        Dictionary of inputs, shared by all the calculations. Must not contain
+        `struct_key`.
+    input_struct_key : str
+        Keyword for input structure for `calc`. Default is "struct".
+    final_struct_key : str
+        Key for final structure output from `calc`. Default is "final_structure".
+    max_number_jobs : int
+        Max number of subprocesses running within the WorkGraph. Default is 10.
 
     Returns
     -------
     WorkGraph
-        The workgraph containing the high-throughput workflow.
+        The workgraph ready to be submitted.
     """
-    wg = WorkGraph("ht_workflow")
+    wg = WorkGraph("ht_calculation")
 
     wg.add_task(
-        run_opt_calc, name="opt_task", folder=folder_path, janus_opt_inputs=inputs
+        ht_calc_builder,
+        name="ht_calc",
+        calc=calc,
+        folder=folder,
+        calc_inputs=calc_inputs,
+        input_struct_key=input_struct_key,
+        final_struct_key=final_struct_key,
     )
 
-    wg.group_outputs = [{"name": "opt_structures", "from": "opt_task.final_structures"}]
-
-    wg.to_html()
-
-    wg.max_number_jobs = 10
-
-    wg.submit()
+    wg.group_outputs = [
+        {"name": "final_structures", "from": "ht_calc.final_structures"}
+    ]
+    wg.max_number_jobs = max_number_jobs
 
     return wg
