e321f2b4b2b6bd720921d666ea4cc10690449451
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2024-11-21 16:46:53 +0100

    Update example ht submit

--- examples/workflows/submit_ht_workgraph.py
+++ examples/workflows/submit_ht_workgraph.py
@@ -3,18 +3,28 @@
 from pathlib import Path
 
 from aiida.orm import load_code
+from aiida.plugins import CalculationFactory
 
 from aiida_mlip.data.model import ModelData
-from aiida_mlip.workflows.ht_workgraph import ht_workgraph
+from aiida_mlip.workflows.ht_workgraph import get_ht_workgraph
 
-folder_path = Path("/home/federica/aiida-mlip/tests/workflows/structures/")
+SinglepointCalc = CalculationFactory("mlip.sp")
+
 inputs = {
     "model": ModelData.from_local(
-        "/home/federica/aiida-mlip/tests/data/input_files/mace/mace_mp_small.model",
+        "./tests/calculations/configs/test.model",
         architecture="mace_mp",
     ),
     "metadata": {"options": {"resources": {"num_machines": 1}}},
     "code": load_code("janus@localhost"),
 }
 
-ht_workgraph(folder_path, inputs)
+wg = get_ht_workgraph(
+    calc=SinglepointCalc,
+    folder=Path("./tests/workflows/structures/"),
+    calc_inputs=inputs,
+    final_struct_key="xyz_output",
+    max_number_jobs=10,
+)
+
+wg.submit()
